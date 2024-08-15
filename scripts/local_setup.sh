31b88dfb7f59293c2762375cb8e5ed31999880ee
Arturo <arti.ramos.s@gmail.com>
2024-08-16 09:15:35 +1000

    Adding local set up

---
+++ scripts/local_setup.sh
@@ -0,0 +1,5 @@
+# first run assuming you have conda
+#conda create -n py39 python=3.9
+#conda activate py39
+pip config set global.trusted-host 'pypi.org files.pythonhosted.org pypi.python.org'
+pipenv install
