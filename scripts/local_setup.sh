641c54359658f265e6637412d11306f0fb1195eb
Arturo <arti.ramos.s@gmail.com>
2024-08-21 10:03:56 +1000

    pre commit set up

--- scripts/local_setup.sh
+++ scripts/local_setup.sh
@@ -1,5 +1,4 @@
 # first run assuming you have conda
-#conda create -n py39 python=3.9
-#conda activate py39
+# Require python 3.12
 pip config set global.trusted-host 'pypi.org files.pythonhosted.org pypi.python.org'
 pipenv install
